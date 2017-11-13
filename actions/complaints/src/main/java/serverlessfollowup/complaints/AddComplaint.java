e3039b414230e0a0300394fd6bcfe2ebf4f8bd70
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-11-13 11:00:34 +0100

    store complaints

--- actions/complaints/src/main/java/serverlessfollowup/complaints/AddComplaint.java
+++ actions/complaints/src/main/java/serverlessfollowup/complaints/AddComplaint.java
@@ -1,14 +1,47 @@
 package serverlessfollowup.complaints;
 
+import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.cloudant.client.api.ClientBuilder;
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+import com.cloudant.client.api.model.Response;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class AddComplaint {
-    public static JsonObject main(JsonObject args) {
-        String name = "stranger";
-        if (args.has("name"))
-            name = args.getAsJsonPrimitive("name").getAsString();
-        JsonObject response = new JsonObject();
-        response.addProperty("greeting", "Hello " + name + "!");
-        return response;
+
+  public static JsonObject main(JsonObject args) throws Exception {
+    // read the tokens
+    JsonObject accessToken = (JsonObject) args.get("_accessToken");
+    
+    // if no access token, return error
+    if (accessToken == null) {
+      throw new IllegalArgumentException("Missing access token");
     }
+
+    // extract the subject from the token
+    String subject = accessToken.getAsJsonPrimitive("sub").getAsString();
+
+    // store the complaints
+    CloudantClient client = ClientBuilder.url(new URL(args.getAsJsonPrimitive("services.cloudant.url").getAsString()))
+        .build();
+    Database complaints = client.database("complaints", true);
+
+    Map<String, Object> newComplaint = new LinkedHashMap<String, Object>();
+    newComplaint.put("subject", subject);
+    newComplaint.put("message", args.getAsJsonPrimitive("message").getAsString());
+    Response newComplaintResponse = complaints.save(newComplaint);
+
+    JsonObject status = new JsonObject();
+    status.addProperty("ok", newComplaintResponse.getError() == null);
+    status.add("response", new Gson().toJsonTree(newComplaintResponse));
+    
+    JsonObject body = new JsonObject();
+    body.add("body", status);
+    return body;
+  }
+
 }
